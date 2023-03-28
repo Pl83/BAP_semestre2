80fc1f9dcd34ee5708d5329494c128f266a7aa28
MEHDI BELLAM <mehdi.bellam@outlook.fr>
2023-03-28 15:26:26 +0000

    :sparkles: slider now work :sparkles:

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const contBtnRight = document.querySelector(".cont-btn-right");
 const contBtnLeft = document.querySelector(".cont-btn-left");
+
+const containerCards = document.querySelectorAll(".container-card");
+console.log(containerCards);
 let index = 0;
 
 
@@ -10,8 +13,14 @@ contBtnLeft.addEventListener("click", () => {
   console.log(index);
    if (index == 0) {
      contBtnLeft.style.visibility = "hidden";
-   }
+  }
+ 
   
+   containerCards.forEach((containerCard) => {
+     console.log(containerCard);
+     containerCard.style.transform = "translateX(0)";
+   });
+
   
 });
 
@@ -23,7 +32,11 @@ contBtnRight.addEventListener("click", () => {
   if (index != 0) {
     contBtnLeft.style.visibility = "visible";
   }
- 
+  containerCards.forEach((containerCard) => {
+    console.log(containerCard);
+    containerCard.style.transform = "translateX(-1050px)";
+  });
+
  
 });
 
