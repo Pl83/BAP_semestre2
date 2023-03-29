7284b7264d52df6e664cd431848ccd4f3d01413f
MEHDI BELLAM <porcepic91@gmail.com>
2023-03-29 17:12:21 +0200

    avancement et ajout de la deuxieme slider

--- script.js
+++ script.js
@@ -1,31 +1,62 @@
+const contBtnRightData = document.querySelector(".cont-btn-right-data");
+const contBtnLeftData = document.querySelector(".cont-btn-left-data");
+
+const containerCardsData = document.querySelectorAll(".container-card-data");
+
 const contBtnRight = document.querySelector(".cont-btn-right");
 const contBtnLeft = document.querySelector(".cont-btn-left");
 
 const containerCards = document.querySelectorAll(".container-card");
-console.log(containerCards);
+
+let indexData = 0;
 let index = 0;
 
 
-contBtnLeft.addEventListener("click", () => { 
+contBtnLeftData.addEventListener("click", () => {
+  console.log("left" + "Data");
+  indexData--;
+  console.log(indexData + "Data");
+  if (indexData == 0) {
+    contBtnLeftData.style.visibility = "hidden";
+  }
 
+  containerCardsData.forEach((containerCard) => {
+    console.log("data" + containerCard);
+    containerCard.style.transform =
+      "translateX(" + -1070 * indexData + "px)";
+  });
+});
+
+contBtnRightData.addEventListener("click", () => {
+  console.log("right Data");
+  indexData++;
+  console.log(indexData +"Data");
+  if (indexData != 0) {
+    contBtnLeftData.style.visibility = "visible";
+  }
+  containerCardsData.forEach((containerCard) => {
+    console.log(containerCard);
+    containerCard.style.transform = "translateX(" + -1070 * indexData + "px)";
+  });
+});
+
+
+contBtnLeft.addEventListener("click", () => {
   console.log("left");
   index--;
   console.log(index);
-   if (index == 0) {
-     contBtnLeft.style.visibility = "hidden";
+  if (index == 0) {
+    contBtnLeft.style.visibility = "hidden";
   }
- 
-  
-   containerCards.forEach((containerCard) => {
-     console.log(containerCard);
-     containerCard.style.transform = "translateX(0)";
-   });
 
-  
+  containerCards.forEach((containerCard) => {
+    console.log(containerCard);
+    containerCard.style.transform =
+      "translateX(" + -1070 * index + "px)";
+  });
 });
 
-contBtnRight.addEventListener("click", () => { 
-
+contBtnRight.addEventListener("click", () => {
   console.log("right");
   index++;
   console.log(index);
@@ -34,9 +65,8 @@ contBtnRight.addEventListener("click", () => {
   }
   containerCards.forEach((containerCard) => {
     console.log(containerCard);
-    containerCard.style.transform = "translateX(-1050px)";
+    containerCard.style.transform = "translateX(" + -1070 * index+ "px)";
   });
-
- 
 });
+
 
