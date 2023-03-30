ff0c89992463530623bc18e218c84a5e039bab9a
MEHDI BELLAM <mehdi.bellam@outlook.fr>
2023-03-30 08:26:09 +0200

    suppresion du boutton scroll right si on arrive a la fin des données

--- script.js
+++ script.js
@@ -9,40 +9,47 @@ const contBtnLeft = document.querySelector(".cont-btn-left");
 const containerCards = document.querySelectorAll(".container-card");
 
 let indexData = 0;
+let IndexDataMax = containerCardsData.length 
+
 let index = 0;
+let IndexMax = containerCards.length; 
 
 
 contBtnLeftData.addEventListener("click", () => {
-  console.log("left" + "Data");
+  console.log("left" + "Data" + containerCardsData.length + containerCardsData);
   indexData--;
-  console.log(indexData + "Data");
+
   if (indexData == 0) {
     contBtnLeftData.style.visibility = "hidden";
   }
 
   containerCardsData.forEach((containerCard) => {
-    console.log("data" + containerCard);
+   
     containerCard.style.transform =
       "translateX(" + -1070 * indexData + "px)";
   });
 });
 
 contBtnRightData.addEventListener("click", () => {
-  console.log("right Data");
+  console.log("right Data" + containerCardsData.length + containerCardsData);
   indexData++;
   console.log(indexData +"Data");
   if (indexData != 0) {
     contBtnLeftData.style.visibility = "visible";
   }
+  if (((indexData+1)*4) >= IndexDataMax ) {
+    console.log("indexData+1)*4) == IndexDataMax");
+    contBtnRightData.style.visibility = "hidden";
+  }
   containerCardsData.forEach((containerCard) => {
-    console.log(containerCard);
+    
     containerCard.style.transform = "translateX(" + -1070 * indexData + "px)";
   });
 });
 
 
 contBtnLeft.addEventListener("click", () => {
-  console.log("left");
+  console.log("left" + containerCardsData.length + containerCardsData);
   index--;
   console.log(index);
   if (index == 0) {
@@ -50,21 +57,21 @@ contBtnLeft.addEventListener("click", () => {
   }
 
   containerCards.forEach((containerCard) => {
-    console.log(containerCard);
+    
     containerCard.style.transform =
       "translateX(" + -1070 * index + "px)";
   });
 });
 
 contBtnRight.addEventListener("click", () => {
-  console.log("right");
+  console.log("right" + containerCardsData.length + containerCardsData);
   index++;
   console.log(index);
   if (index != 0) {
     contBtnLeft.style.visibility = "visible";
   }
   containerCards.forEach((containerCard) => {
-    console.log(containerCard);
+   
     containerCard.style.transform = "translateX(" + -1070 * index+ "px)";
   });
 });
